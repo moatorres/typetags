5a66d1517b16be757276bd9b3499e10fbf482942
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-30 15:02:34 -0300

    new: rollup config file

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+import babel from '@rollup/plugin-babel'
+import pkg from './package.json'
+
+const input = ['lib/index.js']
+
+export default [
+  {
+    input,
+    plugins: [
+      nodeResolve(),
+      babel({
+        babelHelpers: 'bundled',
+        include: ['lib/**/*.d.ts'],
+      }),
+      terser(),
+    ],
+    output: {
+      file: `dist/umd/${pkg.name}.min.js`,
+      format: 'umd',
+      name: 'typetags',
+      esModule: false,
+      exports: 'named',
+      sourcemap: true,
+    },
+  },
+
+  {
+    input,
+    plugins: [nodeResolve()],
+    output: [
+      {
+        dir: 'dist/esm',
+        format: 'esm',
+        exports: 'named',
+        sourcemap: true,
+      },
+      {
+        dir: 'dist/cjs',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+  },
+]
