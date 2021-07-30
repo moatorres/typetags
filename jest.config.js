713530d2533be3b8ad2cf5bf9810e6d38384fd1d
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-30 15:02:22 -0300

    update: finished setup

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'node',
+  testRegex: '\\.(test|spec)?\\.(js|jsx)$',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  setupFilesAfterEnv: ['jest-extended'],
+}
