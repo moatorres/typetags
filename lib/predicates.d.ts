0b85c2807d373653ab4d9de661de0a3690e4960d
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-30 15:06:17 -0300

    new: predicater with decl. and tests

---
+++ lib/predicates.d.ts
@@ -0,0 +1,5 @@
+import { Tags } from './type-tags'
+
+export declare const tags: Tags
+export declare function isDefaultTag(str: string): boolean
+export declare function hasDefaultTag(v: any): boolean
