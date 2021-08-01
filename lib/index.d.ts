4fd300836711635998f68839a47acd5de0a47467
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-31 22:44:39 -0300

    update: fixed imports

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 export { TypeTags } from './type-tags'
-export type { Types, Tags, ITypeTags } from './type-tags'
+export type { Types, Tags, TypeTagsEnum } from './type-tags'
 export { default as getTag } from './get-tag'
 export * from './predicates'
